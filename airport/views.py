583457934c6b7a24c61aa57ea793e297794182cb
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-30 21:29:49 +0300

    add to views airplane and airplane type viewsets

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+from airport.models import AirplaneType, Airplane
+from airport.serializers import (
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    AirplaneListSerializer,
+    AirplaneRetrieveSerializer
+)
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.select_related("airplane_type")
+    serializer_class = AirplaneSerializer
+
+    def get_serializer_class(self) -> ModelSerializer:
+        serializer = super().get_serializer_class()
+        if self.action == "list":
+            serializer = AirplaneListSerializer
+        elif self.action == "retrieve":
+            serializer = AirplaneRetrieveSerializer
+
+        return serializer
