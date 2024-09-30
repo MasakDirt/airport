c87572da92be30c161563c1c6f09b623c3a97ddd
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-30 21:30:14 +0300

    add to airport urls viewsets

--- airport/urls.py
+++ airport/urls.py
@@ -1,3 +1,20 @@
-app_name = "airport"
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from airport.views import AirplaneTypeViewSet, AirplaneViewSet
+
+
+router = DefaultRouter()
+
+router.register(
+    "airplane_types",
+    AirplaneTypeViewSet,
+    basename="airplane_type"
+)
+router.register("airplanes", AirplaneViewSet, basename="airplane")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "airport"
