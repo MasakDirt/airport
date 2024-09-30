aac27434ae1b82f575e91054ac700a1d5c244d0e
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-30 21:27:35 +0300

    add image fields to airplane model

--- airport/models.py
+++ airport/models.py
@@ -1,9 +1,12 @@
+import os.path
+import uuid
 from typing import Callable
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import UniqueConstraint
+from django.utils.text import slugify
 
 
 class AirplaneType(models.Model):
@@ -16,10 +19,18 @@ class AirplaneType(models.Model):
         return self.name
 
 
+def airplane_image(instance: "Airplane", filename: str) -> str:
+    filename, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}-{filename}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
 class Airplane(models.Model):
     name = models.CharField(max_length=255)
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
+    image = models.ImageField(null=True, upload_to=airplane_image)
     airplane_type = models.ForeignKey(
         AirplaneType,
         on_delete=models.CASCADE,
@@ -75,10 +86,16 @@ class Crew(models.Model):
 
 
 class Flight(models.Model):
-    route = models.ForeignKey(Route, on_delete=models.CASCADE,
-                              related_name="flights")
-    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE,
-                                 related_name="flights")
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
 
