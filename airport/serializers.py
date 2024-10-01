ae2da0f6f2c2b085fcd321a4c537f11cfd9d092f
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-01 20:57:56 +0300

    create serializers for crew model and flight

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from airport.models import AirplaneType, Airplane, Airport, Route, Crew
+from airport.models import AirplaneType, Airplane, Airport, Route, Crew, Flight
 
 
 class AirplaneTypeSerializer(serializers.ModelSerializer):
@@ -75,3 +76,70 @@ class RouteListSerializer(RouteSerializer):
 class RouteDetailSerializer(RouteSerializer):
     source = AirportSerializer(read_only=True)
     destination = AirportSerializer(read_only=True)
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    route = serializers.PrimaryKeyRelatedField(
+        queryset=Route.objects.select_related("source", "destination")
+    )
+    airplane = serializers.PrimaryKeyRelatedField(
+        queryset=Airplane.objects.select_related("airplane_type")
+    )
+
+    class Meta:
+        model = Flight
+        fields = ("id", "route", "airplane", "departure_time", "arrival_time")
+
+    def validate(self, attrs: dict) -> dict:
+        Flight.validate_time(
+            departure_time=attrs["departure_time"],
+            arrival_time=attrs["arrival_time"],
+            error_to_raise=ValidationError
+        )
+        return attrs
+
+
+class FlightDetailSerializer(FlightSerializer):
+    route = RouteListSerializer(read_only=True)
+    airplane = AirplaneListSerializer(read_only=True)
+
+
+class FlightListSerializer(serializers.ModelSerializer):
+    airplane_name = serializers.SlugRelatedField(
+        source="airplane",
+        slug_field="name",
+        read_only=True
+    )
+    airplane_type = serializers.SlugRelatedField(
+        source="airplane.airplane_type",
+        slug_field="name",
+        read_only=True
+    )
+    _from = serializers.SlugRelatedField(
+        source="route.source",
+        slug_field="name",
+        read_only=True
+    )
+    to = serializers.SlugRelatedField(
+        source="route.destination",
+        slug_field="name",
+        read_only=True
+    )
+
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "_from",
+            "to",
+            "airplane_name",
+            "airplane_type",
+            "departure_time",
+            "arrival_time"
+        )
