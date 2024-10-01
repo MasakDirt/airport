08053c3dc22a875b41df034e9731c4262028e3c2
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-01 23:22:03 +0300

    add to main urls media urls and debug

--- airport_core/urls.py
+++ airport_core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -24,4 +25,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/airport/", include("airport.urls", namespace="airport")),
     path("api/v1/user/", include("user.urls", namespace="user")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
