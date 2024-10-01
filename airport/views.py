f2ca3bdb788bcac7be843cbb511083fa801284ad
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-01 11:51:25 +0300

    add views for Airport and Route

--- airport/views.py
+++ airport/views.py
@@ -1,12 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.serializers import ModelSerializer
 
-from airport.models import AirplaneType, Airplane
+from airport.models import AirplaneType, Airplane, Airport, Route
 from airport.serializers import (
     AirplaneTypeSerializer,
     AirplaneSerializer,
     AirplaneListSerializer,
-    AirplaneRetrieveSerializer
+    AirplaneRetrieveSerializer,
+    AirportSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    AirportDetailSerializer,
+    RouteDetailSerializer
 )
 
 
@@ -25,5 +30,33 @@ class AirplaneViewSet(viewsets.ModelViewSet):
             serializer = AirplaneListSerializer
         elif self.action == "retrieve":
             serializer = AirplaneRetrieveSerializer
+
+        return serializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+    def get_serializer_class(self) -> ModelSerializer:
+        serializer = super().get_serializer_class()
+
+        if self.action == "retrieve":
+            serializer = AirportDetailSerializer
+
+        return serializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related("source", "destination")
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self) -> RouteSerializer:
+        serializer = super().get_serializer_class()
+
+        if self.action == "list":
+            serializer = RouteListSerializer
+        elif self.action == "retrieve":
+            serializer = RouteDetailSerializer
 
         return serializer
