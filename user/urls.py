52ba26e4403a23ba5213ae4accc46d12e3f96f97
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-01 23:26:10 +0300

    add all user views to urls file

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+
+from user.views import CreateUserView, CreateTokenView, ManageUserView
+
 app_name = "user"
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="user-register"),
+    path("login/", CreateTokenView.as_view(), name="user-login"),
+    path("me/", ManageUserView.as_view(), name="user-detail")
+]
