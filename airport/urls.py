a1ef1016ef307301185d833849a7c51a45082968
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-01 11:51:47 +0300

    add urls for Airport and Route

--- airport/urls.py
+++ airport/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from airport.views import AirplaneTypeViewSet, AirplaneViewSet
-
+from airport.views import (
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    AirportViewSet,
+    RouteViewSet,
+)
 
 router = DefaultRouter()
 
@@ -12,6 +16,8 @@ router.register(
     basename="airplane_type"
 )
 router.register("airplanes", AirplaneViewSet, basename="airplane")
+router.register("airports", AirportViewSet, basename="airport")
+router.register("routes", RouteViewSet, basename="route")
 
 urlpatterns = [
     path("", include(router.urls))
