03bddf5f9b92ecc4e88dd8c1bb8d0e205b21fb2e
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-02 19:29:09 +0300

    add custom permission asa default to settings

--- airport_core/settings.py
+++ airport_core/settings.py
@@ -149,5 +149,8 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "airport.permissions.IsAdminOrAuthenticatedReadOnly"
     ]
 }
