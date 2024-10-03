38675ff23272af0f575e47ea76fc7c5f3968f2d4
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-03 22:01:21 +0300

    remove from user serializer username field and add style of inputted password

--- user/serializers.py
+++ user/serializers.py
@@ -1,14 +1,21 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.db import transaction
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True, "max_length": 5}}
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "max_length": 5,
+                "style": {"input_type": "password"}
+            }
+        }
 
     def create(self, validated_data: dict) -> get_user_model():
         return get_user_model().objects.create_user(**validated_data)
