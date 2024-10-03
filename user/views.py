93335fb09bdf39a1cfcf4cb70fbec6525868cf02
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-03 22:02:05 +0300

    refactor user view because of adding jwt token authorization

--- user/views.py
+++ user/views.py
@@ -1,20 +1,11 @@
-from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
-    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = ()
-
-
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     permission_classes = ()
 
 
