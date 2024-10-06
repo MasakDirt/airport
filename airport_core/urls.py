e3df47490ef897886dd63e45820c5e4b16f56aa8
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-06 14:06:15 +0300

    add version to documentation paths

--- airport_core/urls.py
+++ airport_core/urls.py
@@ -30,14 +30,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/airport/", include("airport.urls", namespace="airport")),
     path("api/v1/user/", include("user.urls", namespace="user")),
-    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/doc/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/doc/swagger/",
+        "api/v1/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/doc/redoc/",
+        "api/v1/doc/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc"
     ),
